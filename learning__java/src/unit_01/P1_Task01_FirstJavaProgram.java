b54602bfe13c1c36e83f8b6237893dcce42085cc
thequanic <85480632+thequanic@users.noreply.github.com>
2022-03-11 16:38:50 +0530

    second commit

--- learning__java/src/unit_01/P1_Task01_FirstJavaProgram.java
+++ learning__java/src/unit_01/P1_Task01_FirstJavaProgram.java
@@ -3,6 +3,7 @@ package unit_01;
 public class P1_Task01_FirstJavaProgram {
 	
 	public static void main(String[] args) {
+		//hello world program
         System.out.println("Hello, World!"); 
     }
 
