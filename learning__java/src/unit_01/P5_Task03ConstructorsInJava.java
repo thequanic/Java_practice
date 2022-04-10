1398dbbbc65e09d48528a62f970ef86e94ce27c2
thequanic <85480632+thequanic@users.noreply.github.com>
2022-04-10 12:57:02 +0530

    P5 Task03

--- learning__java/src/unit_01/P5_Task03ConstructorsInJava.java
+++ learning__java/src/unit_01/P5_Task03ConstructorsInJava.java
@@ -1,10 +1,51 @@
 package unit_01;
 
+
+/*
+ * Constructor is called when an instance of the class is created
+ * At the time of calling of the constructor, memory for the object is allocated in te memory
+ * It is a special type of method Java
+ * 	-Parameterized Constructor
+ *  -Default Constructor
+ *  
+ */
 public class P5_Task03ConstructorsInJava {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		ABC4 obj= new ABC4();
+		ABC4 obj1= new ABC4(1);
+		
+		System.out.println(obj.a);
+		System.out.println(obj1.a);
 
 	}
 
 }
+
+
+class ABC4
+{
+	int a;
+
+	ABC4()
+	{
+		a=10;
+	}
+	
+	ABC4(int a)
+	{
+		this.a=a;
+	}
+	
+	void display()
+	{
+		int b=10;
+		System.out.println(a);
+		System.out.println(b);
+		
+		return a;
+	}
+}
+
+
