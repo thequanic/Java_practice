fe760aa9981694e0c1fcb4fee5f6678b84d5ff8b
thequanic <85480632+thequanic@users.noreply.github.com>
2022-04-10 12:47:54 +0530

    P5 TASk01 & TASK02

--- learning__java/src/unit_01/P5_Task01_ClassAndObjectInJava.java
+++ learning__java/src/unit_01/P5_Task01_ClassAndObjectInJava.java
@@ -3,8 +3,34 @@ package unit_01;
 public class P5_Task01_ClassAndObjectInJava {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		ABC2 obj = new ABC2();
+		
+		obj.display2();
+		obj.display();
 
 	}
 
 }
+
+class ABC2
+{
+	int a=10;
+	static int b=10;
+	
+	static void display()
+	{
+		int b=10;
+		b++;
+		
+		System.out.println(b);
+	}
+	
+	void display2()
+	{
+		System.out.println(a);
+	
+	}
+}
+
+
--- learning__java/src/unit_01/P5_Task02_MethodsInJava.java
+++ learning__java/src/unit_01/P5_Task02_MethodsInJava.java
@@ -3,8 +3,42 @@ package unit_01;
 public class P5_Task02_MethodsInJava {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ABC3 obj= new ABC3();
+		
+		//calling display method definition
+		obj.display();
+		System.out.println(obj.display2());
 
 	}
 
+}
+
+class ABC3
+{
+	int a = 10;
+	
+	/*
+	 * Actual method definition
+	 * 
+	 * DataType methodName (DataType2 p1, DataType2 p2)
+	 * 
+	 * methodBody;
+	 * 
+	 * return value; //if required
+	 * 
+	 */
+	
+	static void display()
+	{
+		int b=10;
+		int a =10;
+		System.out.println(b);
+		System.out.println(a);
+	}
+	
+	int display2()
+	{
+		System.out.println(a);
+		return a;
+	}
 }
