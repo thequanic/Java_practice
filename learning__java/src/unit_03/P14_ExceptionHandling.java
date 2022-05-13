d11095fe21f3edcfb753e039d7ac7268d7a0b3e5
thequanic <85480632+thequanic@users.noreply.github.com>
2022-05-13 16:14:10 +0530

    p14

--- learning__java/src/unit_03/P14_ExceptionHandling.java
+++ learning__java/src/unit_03/P14_ExceptionHandling.java
@@ -72,8 +72,10 @@ public class P14_ExceptionHandling {
 		obj.indexOutOfBoundException();
 		//obj.stackOverFlowError();
 		obj.nullPointerException();
-		//obj.useOfThrow();
-		obj.useOfThrowsAndFinally();
+		obj.useOfThrow(12);
+		obj.useOfFinally();
+		obj.checkedAndUncheckedException();
+		obj.usingMyExeption(-7);
 	
 		
 		System.out.println("Program Done!");
@@ -166,7 +168,7 @@ class ExceptionsInJava
 			a= scan.nextInt();
 			
 			System.out.println("Entered number is:"+a);
-			//scan.close();
+			scan.close();
 		}
 		catch(InputMismatchException e)
 		{
@@ -231,15 +233,52 @@ class ExceptionsInJava
 		System.out.println("Done!");
 	}
 	
-	void useOfThrow(int age) throws Exception
+	void useOfThrow(int age) 
 	{
 		//throw keyword is used in java to explicitly throw an exception from a method or any block of code
 		//we can either throw checked or unchecked exception
 		//the throw keyword is mainly used to throw custom exception
+		try
+		{
+
+			if(age<18)
+			{
+				throw new ArithmeticException("Use of Throw");
+
+				//this error must be handled by caller method
+				//pointed by throws
+			}
+			else
+			{
+				System.out.println("You are allowed");
+			}
+		}
+		catch(ArithmeticException e)
+		{
+			System.out.println(e.getMessage());
+		}
+			
+		
+		
+		System.out.println("Done!");
+	}
+	
+	void useOfThrows(int age) throws Exception
+	{
+		//throws is a keyword in java which is used in the signature 
+		//of method to indicated that this method might throw one of the listed type exceptions
+						
+						/*
+						 * we can use throws keyword to delegate the responsibility of 
+						 * exception handling to the caller then caller method is responsible to handle that exception
+						 * 
+						 * syntax:
+						 * type method_name(parameters) throws exception_list
+						 */
 		
 			if(age<18)
 			{
-				throw new Exception("Use of Throw");
+				throw new Exception("Use of Throws");
 
 				//this error must be handled by caller method
 				//pointed by throws
@@ -254,19 +293,10 @@ class ExceptionsInJava
 		System.out.println("Done!");
 	}
 	
-	void useOfThrowsAndFinally() 
+	void useOfFinally() 
 	{
 		
-		//throws is a keyword in java which is used in the signature 
-		//of method to indicated that this method might throw one of the listed type exceptions
-				
-				/*
-				 * we can use throws keyword to delegate the responsibility of 
-				 * exception handling to the caller then caller method is responsible to handle that exception
-				 * 
-				 * syntax:
-				 * type method_name(parameters) throws exception_list
-				 */
+		
 		
 		/*
 		 * finally	The "finally" block is used to execute the necessary code of the program. 
@@ -276,7 +306,7 @@ class ExceptionsInJava
 		try
 		{
 			
-			useOfThrow(12);
+			useOfThrows(12);
 			
 			//we can not use it without try block as it throws checked exception
 			//so compiler enforce use to handle it
@@ -295,4 +325,103 @@ class ExceptionsInJava
 		
 
 	}
+	
+	void checkedAndUncheckedException()
+	{
+		/*
+		 *There are mainly two types of exceptions: checked and unchecked. 
+		 *An error is considered as the unchecked exception. 
+		 *However, according to Oracle, there are three types of exceptions namely:
+		
+		 *1.Checked Exception:
+		 *-The classes that directly inherit the Throwable class except RuntimeException and Error are known as checked exceptions. 
+		 *-For example, IOException, SQLException, etc.
+		 *-Checked exceptions are checked at compile-time.
+		 *
+		 *2.Unchecked Exception:
+		 *-The classes that inherit the RuntimeException are known as unchecked exceptions. 
+		 *-For example, ArithmeticException, NullPointerException, ArrayIndexOutOfBoundsException, etc. 
+		 *-Unchecked exceptions are not checked at compile-time, but they are checked at runtime.
+		 *
+		 *3.Error:
+		 *-Error is irrecoverable. Some example of errors are OutOfMemoryError, VirtualMachineError, AssertionError etc.
+		 *-Errors cannot be handled
+		 *-for example when stackOverFlow occurs we can not handle it as memory can not be increased
+		 *
+		 */
+		
+		try
+		{
+			
+			useOfThrow(12);
+			
+			//checked Exception
+			
+			//we can not use it without try block as it throws checked exception
+			//so compiler enforce use to handle it
+			//if it would have thrown ArithmeticException then it can be called without try
+			//as it is a unchecked exception
+		}
+		catch(Exception e)
+		{
+			System.out.println("checked Exception");
+		}
+		
+		try
+		{
+			int b=0;
+			int a=5/b;                          //code breaks at this line and exception is thrown
+			System.out.println("5/0="+a);
+			//unchecked Exception
+		}
+		catch(ArithmeticException e)
+		{
+			//Thrown when an exceptional arithmetic condition has occurred.
+			//For example, an integer "divide by zero" throws an instance of this class
+			
+			//catch the occurred exception
+			
+		
+			System.out.println("unchecked Exception");
+			
+			
+		}
+		System.out.println("Done!");
+	}
+	
+	void usingMyExeption(int a)
+	{
+		try
+		{
+			if(a<0)
+			{
+				throw new MyException();
+			}
+			else
+			{
+				System.out.println(a+"is positive");
+			}
+		}
+		catch(MyException e)
+		{
+			System.out.println(e.getMessage());
+		}
+	}
+	
+
 }
+
+//making own exception
+
+class MyException extends Exception
+{
+	//to create our own exception our class should extend Exception Class
+	public String getMessage()
+	{
+		
+		//we can override getMessage method of Exception Class
+		return "MyException Occured";
+	}
+}
+
+
