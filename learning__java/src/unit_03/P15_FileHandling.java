50d65a9789a6883416780abfacb6c741e93703a5
thequanic <85480632+thequanic@users.noreply.github.com>
2022-05-17 11:12:02 +0530

    commit

---
+++ learning__java/src/unit_03/P15_FileHandling.java
@@ -0,0 +1,34 @@
+package unit_03;
+
+public class P15_FileHandling {
+
+	public static void main(String[] args) {
+		
+		
+		FileHandlingInJava obj= new FileHandlingInJava();
+		obj.takeInputFromAFileNotFromConsole();
+		obj.saveOutputIntoFile();
+		obj.createALogFile();
+		
+
+	}
+
+}
+
+class FileHandlingInJava
+{
+	void takeInputFromAFileNotFromConsole()
+	{
+		
+	}
+	
+	void saveOutputIntoFile()
+	{
+		
+	}
+	
+	void createALogFile()
+	{
+		
+	}
+}
---
+++ learning__java/src/unit_03/P16_MultiThreading.java
@@ -0,0 +1,43 @@
+package unit_03;
+
+public class P16_MultiThreading {
+		public static void main(String[] args)
+		{
+			
+			MultiThreadingInJava obj = new MultiThreadingInJava();
+			obj.threadConcept();
+			obj.threadSynchronization();
+			obj.interThreadCommunication();
+			obj.ProducerConsumerProblems();
+			obj.Wait_And_Notify();
+		}
+}
+
+
+class MultiThreadingInJava
+{
+	void threadConcept()
+	{
+		
+	}
+	
+	void threadSynchronization()
+	{
+		
+	}
+	
+	void interThreadCommunication()
+	{
+		
+	}
+	
+	void ProducerConsumerProblems()
+	{
+		
+	}
+	
+	void Wait_And_Notify()
+	{
+		
+	}
+}
